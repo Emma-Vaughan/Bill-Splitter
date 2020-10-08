411f75a95926906f5b749ce2fb029347c2e9ad38
Emma <Emma@Stephens-MacBook-Air.local>
2020-10-08 16:28:33 +0100

    Added extra JS

--- JSProject.js
+++ JSProject.js
@@ -1,6 +1,27 @@
+function isThisMoney(amount) {
+  const pennies = amount * 100;
+  const roundUp = Number.isInteger(pennies);
+  console.log(roundUp);
+  return roundUp;
+}
+
 calculate = function () {
   const amount = document.getElementById("enteramount").value;
-  const people = document.getElementById("numberofpeople").value;
-  const result = amount / people;
-  document.getElementById("finalamount").innerHTML = result;
+  if (amount < 0) {
+    document.getElementById("finalamount").innerHTML =
+      "Looks like you entered a minus number and didn't spent any money today - yay savings!";
+  } else if (isThisMoney(amount) !== true) {
+    document.getElementById("finalamount").innerHTML =
+      "Please enter a valid monetary amount";
+  } else {
+    const people = document.getElementById("numberofpeople").value;
+    if (people < 1) {
+      document.getElementById("finalamount").innerHTML =
+        "At least 1 person must be paying, dickhead!";
+    } else {
+      const result = parseInt(amount) / parseInt(people);
+      const display = "£" + result.toFixed(2) + " p.p.";
+      document.getElementById("finalamount").innerHTML = display;
+    }
+  }
 };
